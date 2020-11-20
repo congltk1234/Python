fb32365041cb55935c33e458c3e4e6f2521f07e7
congltk1234 <73183412+congltk1234@users.noreply.github.com>
2020-11-21 01:01:11 +0700

    Update bai9.py

--- bai9.py
+++ bai9.py
@@ -1,11 +1,11 @@
 import os
 import time
 while True :
-    check = input("Want to shutdown your computer ? (y/n): ");
+    check = input("Bạn có muốn tắt máy không? (y/n): "); 
     if check == 'n':
-        for c in range(1 , 31):
-            print(c, end=' ')
+        for c in range(1 , 31): 
+            print(c, end=' ')   #Hiển thị số giây
             time.sleep(1) #Hen gio tat may (30s)
     else:
-        os.system("shutdown /s /t 1");
+        os.system("shutdown /s /t 1"); #Tắt máy trong 1s
         break
